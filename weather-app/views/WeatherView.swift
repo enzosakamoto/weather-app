4f8b473633be6057e7fa87513bb64797936866df
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-12-06 20:51:14 -0300

    added new image

--- weather-app/views/WeatherView.swift
+++ weather-app/views/WeatherView.swift
@@ -58,13 +58,10 @@ struct WeatherView: View {
                     Spacer()
                         .frame(height: 40)
                     
-                    AsyncImage(url: URL(string: "https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png")) { image in image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 350)
-                    } placeholder: {
-                        ProgressView()
-                    }
+                    Image(.city)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 250)
                     
                     Spacer()
                     
